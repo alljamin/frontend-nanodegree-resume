7cffe0617982c87ae448d34b7b1b023866d23adb
Aleksandr Ljamin <aleksandr@commondore.gateway>
2016-09-24 14:01:34 +0930

    deleted Hello world and added name and role details

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+$("#header").append()
+
+
+
+var email = "name@yahoo.com";
+var newEmail = 
+    email.replace("yahoo", "gmail");
+console.log(email);
+console.log(newEmail);
+
+// var awesomeThoughts = "My name is Alex and I am AWESOME!";
+
+// var funThoughts = 
+//     awesomeThoughts.replace("AWESOME", "FUN");
+// $("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%","Aleksandr Ljamin");
+var formattedRole = HTMLheaderRole.replace("%data%","web designer");
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
